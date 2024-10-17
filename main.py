ea8874146f4f5e4d8969c1bda09080d4c4d59670
bykovskiy_m_r <misha.bykovskij@bk.ru>
2024-10-17 21:37:46 +0300

    csv data

--- main.py
+++ main.py
@@ -1,37 +1,3 @@
-# import os
-
-# import pandas as pd
-# from dotenv import load_dotenv
-
-# from app.models.yandexgpt import YandexGPT
-# from app.utils.submit import generate_submit
-
-# if __name__ == "__main__":
-#     load_dotenv()
-
-#     system_prompt = """
-#     Ты - профессиональный программист и ментор. Давай очень короткие ответы о синтаксических ошибках в коде, если они есть.
-#     """
-
-#     yandex_gpt = YandexGPT(
-#         token=os.environ["YANDEX_GPT_IAM_TOKEN"],
-#         folder_id=os.environ["YANDEX_GPT_FOLDER_ID"],
-#         system_prompt=system_prompt,
-#     )
-
-
-#     def predict(row: pd.Series) -> str:
-#         return yandex_gpt.ask(row["student_solution"])
-
-
-#     generate_submit(
-#         test_solutions_path="../data/raw/test/solutions.xlsx",
-#         predict_func=predict,
-#         save_path="../data/processed/submission.csv",
-#         use_tqdm=True,
-#     )
-
-
 import os
 import pandas as pd
 from typing import List
@@ -42,8 +8,8 @@ from app.utils.submit import generate_submit
 
 load_dotenv()
 
-solutions_df = pd.read_excel("data/raw/train/solutions.xlsx", engine='openpyxl')
-tasks_df = pd.read_excel("data/raw/train/tasks.xlsx", engine='openpyxl')
+solutions_df = pd.read_csv("data/raw/train/solutions.csv")
+tasks_df = pd.read_csv("data/raw/train/tasks.csv")
 
 model_name = "IlyaGusev/saiga_llama3_8b"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -76,7 +42,7 @@ def predict(row: pd.Series) -> str:
         return ""
 
 generate_submit(
-    test_solutions_path="../data/raw/test/solutions.xlsx",
+    test_solutions_path="../data/raw/test/solutions.csv",
     predict_func=predict,
     save_path="../data/processed/submission.csv",
     use_tqdm=True,
