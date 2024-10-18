a5e050d7da99fa57ecd5545237be3c7c4a77f3aa
bykovskiy_m_r <misha.bykovskij@bk.ru>
2024-10-18 19:43:44 +0300

    parallel data

--- main.py
+++ main.py
@@ -59,7 +59,12 @@ model_name = "IlyaGusev/saiga_llama3_8b"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-model_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, device=-1)
+# Use multiple GPUs with DataParallel
+model = torch.nn.DataParallel(model, device_ids=[0, 1, 2])
+model.to('cuda')
+
+# Set up the pipeline, using GPU 0 (the first available GPU in DataParallel)
+model_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0)
 
 def get_random_records_as_text(df):
     random_records = df.sample(n=15)
